ca027ff05732ce3ac01b6acdcb12f30fdd0184b0
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-09-03 18:23:59 -0600

    Factory girl experiment_with_factors up and running.

--- spec/factories/experiments.rb
+++ spec/factories/experiments.rb
@@ -1,20 +1,9 @@
 FactoryGirl.define do
   factory :experiment do
     title Faker::Lorem.sentence
-    factory :experiment_with_factors do
-      # posts_count is declared as a transient attribute and available in
-      # attributes on the factory, as well as the callback via the evaluator
-      transient do
-        factors_count 5
-      end
-
-      # the after(:create) yields two values; the user instance itself and the
-      # evaluator, which stores all values from the factory, including transient
-      # attributes; `create_list`'s second argument is the number of records
-      # to create and we make sure the user is associated properly to the post
-      after(:create) do |experiment, evaluator|
-        create_list(:factor, evaluator.factors_count, experiment: experiment)
-      end
-    end
   end
+
+  factory :experiment_with_factors, :parent => :experiment do |experiment|
+    factors { build_list :factor, 3 }
+  end 
 end
