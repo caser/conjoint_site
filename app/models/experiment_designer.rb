6f86e2e12233f942e44db0070a7d35e1994e25a7
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-09-05 21:41:26 -0600

    Full factorial designer is working.

--- app/models/experiment_designer.rb
+++ app/models/experiment_designer.rb
@@ -1,5 +1,50 @@
 class ExperimentDesigner < ActiveRecord::Base
   def self.build_factorial_design(experiment)
     factors = experiment.factors
+    number_of_factors = factors.length
+
+    combinations = ExperimentDesigner.get_all_combinations(factors)
+
+    return combinations
+  end
+
+  def self.get_all_combinations(factors, combinations = [])
+    new_factors = []
+
+    factors.each do |factor|
+      new_factors.push(factor.clone)
+    end
+
+    puts "New factors is:\n #{new_factors.inspect}\n\n"
+    factor = new_factors.shift
+    puts "After POP new factors is:\n #{new_factors.inspect}\n\n"
+
+    new_combinations = []
+
+    unless combinations.empty?
+      # add each level of this factor to the exiting combinations
+      combinations.each do |combination|
+        factor.levels.each do |level|
+          new_combination = combination.clone
+          new_combination.push(level)
+          new_combinations.push(new_combination)
+          # puts "New combinations (recursion!):\n#{new_combinations.inspect}\n\n\n"
+        end
+      end
+    else
+      # initialize combinations array if empty
+      factor.levels.each do |level|
+        new_combinations.push([level])
+      end
+      puts "New combinations (base_case):\n#{new_combinations.inspect}\n\n\n"
+    end
+
+    unless new_factors.empty?
+      combinations = ExperimentDesigner.get_all_combinations(new_factors, new_combinations)
+    else
+      combinations = new_combinations
+    end
+
+    return combinations
   end
 end
--- app/models/factor.rb
+++ app/models/factor.rb
@@ -1,7 +1,18 @@
 class Factor < ActiveRecord::Base
+
   belongs_to :experiment
 
   serialize :levels
 
   validates :title, :levels, presence: true
+
+  def clone
+    new_factor = Factor.new
+
+    new_factor.title = Marshal.load(Marshal.dump(title))
+    new_factor.levels = Marshal.load(Marshal.dump(levels))
+
+    return new_factor
+  end
+
 end
