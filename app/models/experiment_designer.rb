518d68012b3f121249e167bb11a2173766cb93d6
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-09-05 21:45:21 -0600

    Added test for experiment designer.

--- app/models/experiment_designer.rb
+++ app/models/experiment_designer.rb
@@ -15,28 +15,22 @@ class ExperimentDesigner < ActiveRecord::Base
       new_factors.push(factor.clone)
     end
 
-    puts "New factors is:\n #{new_factors.inspect}\n\n"
     factor = new_factors.shift
-    puts "After POP new factors is:\n #{new_factors.inspect}\n\n"
 
     new_combinations = []
 
     unless combinations.empty?
-      # add each level of this factor to the exiting combinations
       combinations.each do |combination|
         factor.levels.each do |level|
           new_combination = combination.clone
           new_combination.push(level)
           new_combinations.push(new_combination)
-          # puts "New combinations (recursion!):\n#{new_combinations.inspect}\n\n\n"
         end
       end
     else
-      # initialize combinations array if empty
       factor.levels.each do |level|
         new_combinations.push([level])
       end
-      puts "New combinations (base_case):\n#{new_combinations.inspect}\n\n\n"
     end
 
     unless new_factors.empty?
--- spec/models/experiment_designer_spec.rb
+++ spec/models/experiment_designer_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe ExperimentDesigner do
-=begin  
-  it "should take an experiment and return a full factorial design" do
-    experiment = build(:experiment)
-    factor1 = build(:color_factor)
-    factor2 = build(:credit_card_factor)
-    factor3 = build(:job_title_factor)
+  describe "##build_factorial_design" do
+    it "should take an experiment and return a full factorial design" do
+      experiment = build(:experiment)
+      factor1 = build(:color_factor)
+      factor2 = build(:credit_card_factor)
+      factor3 = build(:job_title_factor)
 
-    experiment.factors << [factor1, factor2, factor3]
+      experiment.factors << [factor1, factor2, factor3]
 
-    factorial_design = ExperimentDesigner.build_factorial_design(experiment)
+      factorial_design = ExperimentDesigner.build_factorial_design(experiment)
 
-    factor1.levels.each do |level1|
-      factor2.levels.each do |level2|
-        factor3.levels.each do |level3|
-          sample_level = [level1, level2, level3]
-          factorial_design.should include(sample_level)
+      factor1.levels.each do |level1|
+        factor2.levels.each do |level2|
+          factor3.levels.each do |level3|
+            sample_level = [level1, level2, level3]
+            factorial_design.should include(sample_level)
+          end
         end
       end
     end
   end
-=end
 end
